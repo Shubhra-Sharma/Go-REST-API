0beee299f3d8917036d548232fcd41edd4e302c2
Shubhra Sharma <sshubhra093@gmail.com>
2026-04-03 12:12:17 +0000

    Product Service tests added

--- internal/service/productService_test.go
+++ internal/service/productService_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/Shubhra-Sharma/Go-REST-API/internal/domain"
@@ -39,6 +40,53 @@ func TestCreateProduct(t *testing.T) {
 			product: mockProduct(),
 			want:    "",
 		},
+		{ // validation logic tests
+			name:    "missing name",
+			product: &domain.Product{Price: 100, Quantity: 1, Brand: "Vivo", Category: "Device"},
+			want:    "product name is required",
+		},
+		{
+			name:    "zero price",
+			product: &domain.Product{Name: "Phone", Price: 0, Quantity: 1, Brand: "Vivo", Category: "Device"},
+			want:    "product price must be greater than 0",
+		},
+		{
+			name:    "negative price",
+			product: &domain.Product{Name: "Keyboard", Price: -1, Quantity: 1, Brand: "Logitech", Category: "Electronics"},
+			want:    "product price must be greater than 0",
+		},
+		{
+			name:    "negative quantity",
+			product: &domain.Product{Name: "Keyboard", Price: 600, Quantity: -3, Brand: "Logitech", Category: "Electronics"},
+			want:    "quantity must be greater than or equal to 0",
+		},
+		{
+			name:    "missing brand",
+			product: &domain.Product{Name: "Keyboard", Price: 600, Quantity: 1, Brand: "", Category: "Electronics"},
+			want:    "brand name is compulsory for all products",
+		},
+		{
+			name:    "missing category",
+			product: &domain.Product{Name: "Keyboard", Price: 100, Quantity: 1, Brand: "Logitech", Category: ""},
+			want:    "category name is required",
+		},
+		{
+			name:    "zero quantity is valid",
+			product: &domain.Product{Name: "Keyboard", Price: 100, Quantity: 0, Brand: "Logitech", Category: "Electronics"},
+			want:    "",
+		},
+		{ // To check if service is correctly propagating repository errors
+			name:            "category not found in repo",
+			product:         mockProduct(),
+			categoryRepoErr: errors.New("category not found"),
+			want:            "category not found",
+		},
+		{ // To check if service is correctly propagating repository errors
+			name:            "Product repository error",
+			product:         mockProduct(),
+			categoryRepoErr: errors.New("Failed to add new productd"),
+			want:            "Failed to add new product",
+		},
 	}
 
 	for _, tt := range testcases {
@@ -56,16 +104,88 @@ func TestCreateProduct(t *testing.T) {
 			// mock productRepo
 			productRepo := &mockProductRepo{
 				create: func(ctx context.Context, product *domain.Product) error {
-					return tt.productRepoErr
+					if tt.productRepoErr != nil {
+						return tt.productRepoErr
+					}
+					return nil
 				},
 			}
 
 			// creating new service and calling the create function for testing
-			svc := NewProductService(productRepo, categoryRepo)
-			err := svc.CreateProduct(context.Background(), tt.product)
+			service := NewProductService(productRepo, categoryRepo)
+			err := service.CreateProduct(context.Background(), tt.product)
 
 			if tt.want == "" && err != nil {
 				t.Errorf("expected no error, got: %v", err)
+			}
+		})
+	}
+}
+
+func TestUpdateProduct(t *testing.T) {
+	tests := []struct {
+		name           string
+		product        *domain.Product
+		productRepoErr error
+		want           string
+	}{
+		{ // default
+			name:    "success",
+			product: mockProduct(),
+			want:    "",
+		},
+		{ // testing validation logic
+			name:    "missing name",
+			product: &domain.Product{Price: 100, Quantity: 1, Brand: "Logitech"},
+			want:    "product name is required",
+		},
+		{
+			name:    "negative price",
+			product: &domain.Product{Name: "Keyboard", Quantity: 9, Price: -5, Brand: "Logitech"},
+			want:    "product price must be greater than 0",
+		},
+		{
+			name:    "negative quantity",
+			product: &domain.Product{Name: "Keyboard", Quantity: -7, Price: 5, Brand: "Logitech"},
+			want:    "quantity must be greater than or equal to 0",
+		},
+		{
+			name:    "zero price",
+			product: &domain.Product{Name: "Keyboard", Quantity: 9, Price: 0, Brand: "Logitech"},
+			want:    "product price must be greater than 0",
+		},
+		{
+			name:    "missing brand",
+			product: &domain.Product{Name: "Keyboard", Quantity: 9, Price: 2, Brand: ""},
+			want:    "brand name is compulsory for all products",
+		},
+		{ // testing if ProductService correctly propagates repository errors
+			name:           "product not found",
+			product:        mockProduct(),
+			productRepoErr: errors.New("product not found"),
+			want:           "product not found",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// mocking productRepo
+			productRepo := &mockProductRepo{
+				update: func(ctx context.Context, id string, product *domain.Product) error {
+					return tt.productRepoErr
+				},
+			}
+
+			service := NewProductService(productRepo, &mockCategoryRepo{}) // passing empty mockCategory struct since there is no role of category Repo in UpdateProduct function
+			err := service.UpdateProduct(context.Background(), "123", tt.product)
+
+			if tt.want == "" && err != nil {
+				t.Errorf("expected no error, got: %v", err)
+			}
+			if tt.want != "" {
+				if err == nil || err.Error() != tt.want {
+					t.Errorf("expected error '%s', got '%v'", tt.want, err)
+				}
 			}
 		})
 	}
